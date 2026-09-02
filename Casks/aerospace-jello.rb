cdea2221760c5ae46de050d1feb03d39eb52cd49
ManofJELLO <the5hit@gmail.com>
2026-09-02 12:54:59 -0700

    aerospace-jello 0.21.3-jello.21

--- Casks/aerospace-jello.rb
+++ Casks/aerospace-jello.rb
@@ -1,6 +1,6 @@
 cask "aerospace-jello" do
-  version "0.21.3-jello.20"
-  sha256 "16a3655aae7ffa3ad8c275d01fa6ebf905ecf34eb3ba346d7658095dc8a43a76"
+  version "0.21.3-jello.21"
+  sha256 "4e8a432d7a7da550dcecf73d610dd20825543aece7a5404180c8d29f098aceb9"
 
   url "https://github.com/ManofJELLO/AeroSpace_Jello/releases/download/v#{version}/AeroSpace-v#{version}.zip"
   name "AeroSpace (Jello fork)"
